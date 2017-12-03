440ab2a5d940f210dacc5a7f78d3b367c6c27464
Eduardo de Santana Medeiros Alexandre <eduardo.ufpb@gmail.com>
2017-12-03 15:00:06 -0300

    Exportação de TXT

--- app/txts/conversor_txt.rb
+++ app/txts/conversor_txt.rb
@@ -26,28 +26,69 @@ class ConversorTxt
           cUF: '28',
           cNF: '',
           natOp: 'COMPRA PARA COMERCIALIZACAO',
-          indPag: '',
-          mod: '',
-          serie: '',
-          nNF: '',
-          dhEmi: '',
+          indPag: '0',
+          mod: '55',
+          serie: '1',
+          nNF: '7342',
+          dhEmi: '2017-09-28T00:00:00-03:00',
           dhSaiEnt: '',
-          tpNF: '',
-          idDest: '',
-          cMunFG: '',
-          tpImp: '',
-          tpEmis: '',
+          tpNF: '0',
+          idDest: '3',
+          cMunFG: '2806701',
+          tpImp: '1',
+          tpEmis: '1',
           cDV: '',
-          tpAmb: '',
-          finNFe: '',
-          indFinal: '',
-          indPres: '',
-          procEmi: '',
-          verProc: '',
+          tpAmb: '1',
+          finNFe: '1',
+          indFinal: '0',
+          indPres: '2',
+          procEmi: '3',
+          verProc: '3.20.55',
           dhCont: '',
           xJust: ''
-        }
-      
+        },
+      C: {
+        # C|JOAO PAULO PINHEIRO BARONTO-ME|JOAO PAULO|271341432||||1|
+        # C|<%= d.C.xNome %>|<%= d.C.xFant %>|<%= d.C.IE %>|<%= d.C.IEST %>|<%= d.C.IM %>|<%= d.C.CNAE %>|<%= d.C.CRT %>|
+        xNome: 'JOAO PAULO PINHEIRO BARONTO-ME',
+        xFant: 'JOAO PAULO',
+        IE: '271341432',
+        IEST: '',
+        IM: '',
+        CNAE: '',
+        CRT: '1',
+        # C02|14626983000180|
+        # [seleção entre C02 ou C02a]{
+        #  C02|CNPJ|
+        #  [ou]
+        #  C02a|CPF|
+        #  }
+        CNPJ: '14626983000180',
+        CPF: '',
+        # C05|xLgr|nro|xCPL|xBairro|cMun|xMun|UF|CEP|cPais|xPais|fone|
+        # C05|RUA AMINTAS MACHADO DE JESUS|126|SALA E|ROSAELZE|||SE|49100000|1058|BRASIL|7999895550|
+        xLgr: 'RUA AMINTAS MACHADO DE JESUS',
+        nro: '126',
+        xCPL: 'SALA E',
+        xBairro: 'ROSAELZE',
+        cMun: '',
+        xMun: '',
+        UF: 'SE',
+        CEP: '49100000',
+        cPais: '1058',
+        xPais: 'BRASIL',
+        fone: '7999895550'
+      },
+      E: {
+        # E|xNome|indIEDest|IE|ISUF|IM|email|
+        # E|WENZHOU MINGBANG AUTO ACCESSORIES CO.,LTD.|9|||||
+        xNome: 'WENZHOU MINGBANG AUTO ACCESSORIES CO.,LTD.',
+        indIEDest: '9',
+        IE: '',
+        ISUF: '',
+        IM: '',
+        email: ''
+      }
     })
     
     @d = JSON.parse(nota.dados.to_json,object_class: OpenStruct)
@@ -57,8 +98,7 @@ class ConversorTxt
 
   # Documentação do erb: http://ruby-doc.org/stdlib-2.4.2/libdoc/erb/rdoc/ERB.html#method-c-new
   def convert_to_txt
-    @job = ERB.new(File.read(Rails.root + "app/txts/layout_txt-nf-e_v3.10.1.txt.erb"))
+    @job = ERB.new(File.read(Rails.root + "app/txts/layout_txt-nf-e_v3.10.1.txt.erb"), nil, '<>')
     result =  @job.result(binding)
-
   end
 end
